120f5e3804eee54adffdf85da796081e3c56da75
Guillermo Pizarro <guillermo.pizarro.v@gmail.com>
2020-08-25 00:58:16 -0500

    [7]. Se modulariza el código.

--- code/javascript-professional/website/assets/index.js
+++ code/javascript-professional/website/assets/index.js
@@ -1,25 +1,7 @@
+import MediaPlayer from './MediaPlayer.js'
+
 const video = document.querySelector('video')
 const button = document.querySelector('button')
-
-function MediaPlayer(config) {
-  this.media = config.el
-}
-
-MediaPlayer.prototype.play = function() {
-  this.media.play()
-}
-
-MediaPlayer.prototype.pause = function() {
-  this.media.pause()
-}
-
-MediaPlayer.prototype.togglePlay = function() {
-  if (this.media.paused) {
-    this.media.play()
-  } else {
-    this.media.pause()
-  }
-}
 
 const player = new MediaPlayer({ el: video })
 button.onclick = () => player.togglePlay()
